a071b79a68011d5cd6343f9133c2d352e426ce6e
mihasinkec <sinkec.mihael@gmail.com>
2022-09-20 14:36:13 +0200

    Split up to proving and setup part.

--- zk/private-inputs/zokrates/mnist/prove.js
+++ zk/private-inputs/zokrates/mnist/prove.js
@@ -8,30 +8,21 @@ async function run() {
     let source = '';
     let sourceModelParams = '';
     let privInput = null;
+    let pk = null;
     try {
         source = await fs.readFile('circuits/root.zok', { encoding: 'utf8' });
         sourceModelParams = await fs.readFile('circuits/model_params.zok', { encoding: 'utf8' });
         privInput = await fs.readFile('data/witness.json', { encoding: 'utf8' });
         privInput = JSON.parse(privInput);
+        pk = await fs.readFile('data/pk.json', { encoding: 'utf8' });
+        pk = JSON.parse(pk);
     } catch (err) {
         console.log(err);
         process.exit(1);
     }
         
     initialize().then((zokratesProvider) => {
-        // compilation
-        const fileSystemResolver = (from, to) => {
-            let parsedPath = path.parse(
-                path.resolve(path.dirname(path.resolve(from)), to)
-            );
-            const location = path.format({
-                ...parsedPath,
-                base: "",
-                ext: ".zok",
-            });
-            const source = fs.readFileSync(location).toString();
-            return { source, location };
-        };
+        // compile
         const optionsCompile = { 
             config: { debug: true },
             location: "circuits/root.zok",
@@ -42,27 +33,23 @@ async function run() {
         //       the model params import line with the actual source code.
         source = source.replace('from "./model_params" import WEIGHTS_0, WEIGHTS_1, BIASES_0, BIASES_1;', sourceModelParams);
         const artifacts = zokratesProvider.compile(source, optionsCompile);
-    
-        // computation
+
+        //computation
         let inputs = [
             privInput['inputs']
         ];
-
+        
         let logs = [];
         const optionsExecute = {
-              logCallback: (l) => {
-                logs.push(l);
-              },
-            };
+            logCallback: (l) => {
+               logs.push(l);
+            },
+        };
         const { witness, output } = zokratesProvider.computeWitness(artifacts, inputs, optionsExecute);
         console.log(logs);
-
-        // run setup
-        const keypair = zokratesProvider.setup(artifacts.program);
-        fs.writeFile('data/vk.json', JSON.stringify(keypair.vk));
-    
+        
         // generate proof
-        const proof = zokratesProvider.generateProof(artifacts.program, witness, keypair.pk);
+        const proof = zokratesProvider.generateProof(artifacts.program, witness, pk);
         fs.writeFile('data/proof.json', JSON.stringify(proof));
         
     });
